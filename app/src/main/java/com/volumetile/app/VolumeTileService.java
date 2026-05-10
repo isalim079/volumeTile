94f391a3929f2a5671e5fa78042b03a8c791683b
sumonDev249 <sumon.dev@excelbd.com>
2026-05-10 13:59:22 +0600

    feat: update activity launch logic to support Android 14+ PendingIntent requirements

--- app/src/main/java/com/volumetile/app/VolumeTileService.java
+++ app/src/main/java/com/volumetile/app/VolumeTileService.java
@@ -1,6 +1,8 @@
 package com.volumetile.app;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
@@ -21,10 +23,21 @@ public class VolumeTileService extends TileService {
     @Override
     public void onClick() {
         super.onClick();
+
         Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivityAndCollapse(intent);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            PendingIntent pendingIntent = PendingIntent.getActivity(
+                    this,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+            );
+            startActivityAndCollapse(pendingIntent);
+        } else {
+            startActivityAndCollapse(intent);
+        }
     }
 
     private void setTileActive() {
